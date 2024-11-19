7d5c55af52c718a3cd994e353ca008a9b411aaef
LeeSeungJae0503 <48954683+LeeSeungJae0503@users.noreply.github.com>
2024-11-19 12:54:39 +0900

    Update median.py

--- lv_0/median.py
+++ lv_0/median.py
@@ -1,3 +1,6 @@
+# sort는 반환값이 없고 sorted는 반환값이 존재한다. 
+# sorted는 새로운 정렬된 리스트를 반환한다. 
+
 def solution(array):
     array.sort()
     middle = len(array) / 2 - 1 if len(array) % 2 == 0 else len(array) / 2
